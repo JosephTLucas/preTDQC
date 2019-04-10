982480c6cc459226cdd3a10f531682db675f4683
Joshua <joshualawrencemorgan@gmail.com>
2019-04-10 07:35:27 -0400

    morgan added

--- encoder.py
+++ encoder.py
@@ -4,8 +4,13 @@ def lucas(data):
     output = 2*data
     return output
 
+def morgan(data):
+    output = data+2
+    return output
+
 num = int(input("Enter a number: "))
 #Add your function below mine and assign the return value to the variable 'num'
 #We'll create a series of calculations that will be our encoder 
 num = lucas(num)
+num = morgan(num)
 print ("Your encoded number is",num)
