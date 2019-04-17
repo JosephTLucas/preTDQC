86a5e711a89ea43dde98225c1e3a2c0b9c65c19c
mike <mlekhmus@gmail.com>
2019-04-17 03:58:48 -0700

    mike lekhmus

--- encoder.py
+++ encoder.py
@@ -1,11 +1,11 @@
-#Create a function with your name.  Below is an example.
-#Your function should accept an integer, modify it, and return the new integer.
 def lucas(data):
     output = 2*data
     return output
 
+def lekhmus(data):
+    output = 15-data
+    return output
 num = int(input("Enter a number: "))
-#Add your function below mine and assign the return value to the variable 'num'
-#We'll create a series of calculations that will be our encoder 
 num = lucas(num)
+num = lekhmus(num)
 print ("Your encoded number is",num)
